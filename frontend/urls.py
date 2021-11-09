c55964017e4e5f029729b0dcb8e9d39d941b8021
Nigel3005 <57489967+Nigel3005@users.noreply.github.com>
2021-11-09 12:34:05 +0100

    added templates folders

---
+++ frontend/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+    path('', views.indexView, name='Home'),
+    path('login', views.loginView, name="Login"),
+    path('logout', views.logoutView, name="Logout"),
+    path('register', views.registerView, name="Register"),
+    path('profiel', views.profielView, name='Profiel'),
+
+]
--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,59 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
 from django.shortcuts import render
+from forms import SignUpForm, LoginForm
+import datetime as datetime
+import numpy as np
 
-# Create your views here.
+def indexView(request):
+    if not request.user.is_anonymous:
+        template_name = 'index.html'
+        return render(request, 'default.html', {'page': template_name})
+
+def registerView(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('Login')
+    else:
+        form = SignUpForm()
+    return render(request, 'default.html', {'page': 'registration/register.html', 'form': form})
+
+def loginView(request):
+    if request.method == 'POST':
+        try:
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('Home')
+                else:
+                    form = LoginForm
+                    return render(request, 'default.html', {'page': 'registration/login.html', 'form': form, 'error': 'Account is not activated'})
+            else:
+                form = LoginForm
+                return render(request, 'default.html', {'page': 'registration/login.html', 'form': form, 'error': 'Your username and password were incorrect.'})
+        except:
+            form = LoginForm
+            return render(request, 'default.html', {'page': 'registration/login.html', 'form': form, 'error': 'Invalid Form'})
+
+    else:
+        form = LoginForm
+        return render(request, 'default.html', {'page': 'registration/login.html', 'form': form, 'error': ''})
+
+def logoutView(request):
+    logout(request)
+    return redirect('Home')
+
+def profielView(request):
+    if not request.user.is_anonymous:
+        args = {'page': 'profiel.html', 'delta_statusses': None}
+        return render(request, 'default.html', args)
+
